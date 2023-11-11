9c9fe94c342ac6dac3bb1f1051a62c5dacaca9b4
Tulili <tulilirockz@outlook.com>
2023-11-11 20:20:44 -0300

    feat(light): use kde instead of lxqt whatever

--- hosts/light/desktop.nix
+++ hosts/light/desktop.nix
@@ -2,21 +2,29 @@
 
 {
   hardware.pulseaudio.enable = false;
-  
-  xdg.portal.enable = true;
-  xdg.portal.lxqt.enable = true;
-
   services.xserver = {
     enable = true;
     libinput.enable = true;
-    desktopManager.lxqt.enable = true;
+    displayManager.sddm.enable = true;
+    desktopManager.plasma5.enable = true;
+    displayManager.defaultSession = "plasmawayland";
   };
 
   environment.systemPackages = with pkgs; [
+    plasma-pa
+    libsForQt5.kclock
     qpwgraph
     vlc
+    keepassxc
   ];
   
+  environment.plasma5.excludePackages = with pkgs.libsForQt5; [
+    elisa
+    okular
+    oxygen
+    plasma-browser-integration
+  ];
+
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
