d96407d7ae2f5a8b543aae0ede86fd4a3fec8e9f
Tulili <tulilirockz@outlook.com>
2024-02-21 13:49:15 -0300

    feat: minimal configuration for deployments

---
+++ nixos/hosts/minimal/configuration.nix
@@ -0,0 +1,36 @@
+{config,pkgs,...}: {
+  boot = {
+    extraModulePackages = [config.boot.kernelPackages.rtl8192eu];
+    loader.systemd-boot.enable = true;
+    loader.systemd-boot.configurationLimit = 5;
+    loader.efi.canTouchEfiVariables = true;
+  };
+  environment.systemPackages = with pkgs; [
+    just
+    git
+    tmux
+    vim
+  ];
+  services.openssh.enable = true;
+  services.openssh.openFirewall = true;
+  time.timeZone = "America/Sao_Paulo";
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  networking = {
+    hostname = "minimal";
+    networkmanager.enable = true;
+    networkmanager.wifi.backend = "iwd";
+    wireless.enable = pkgs.lib.mkForce false;
+    wireless.iwd.enable = true;
+    wireless.iwd.settings = {
+      Network = {
+        EnableIPV6 = true;
+      };
+      Settings = {
+        AutoConnect = true;
+      };
+    };
+    firewall.enable = false;
+  };
+}
+
